da6364afb828d06bdfd3be2aba66ab4c8d81efeb
Olga Kalyazhinkova <oi.kalyazhinkova@yandex.ru>
2022-10-20 17:47:50 +0300

    Updates

--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export { apiConfig };
-
-const apiConfig = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-48',
-  headers: {
-    authorization: '65de8f77-f8b8-413c-888a-b451adbbaac0',
-    'Content-Type': 'application/json'
-  }
-}; 
